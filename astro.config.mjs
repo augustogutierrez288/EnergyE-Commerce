8a407f08324dbc1d96963f4e0e39308b0864a372
Augusto Gutierrez <augustogutierrez288@gmail.com>
2024-03-01 18:52:03 -0300

    Create a new navbar with hamburger menu. This component is developed with react.js and makes all components and pages responsive on mobile.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
+import netlify from '@astrojs/netlify/edge-functions';
 
 // https://astro.build/config
 export default defineConfig({
   ViewTransitions: true,
   output: "server",
-  integrations: [react()]
+  integrations: [react()],
+  adapter: netlify({
+    cacheOnDemandPages: true,
+  }),
 });
---
+++ src/components/react/Navbar.jsx
@@ -0,0 +1,79 @@
+import { useRef, useEffect, useState } from 'react';
+
+function Navbar() {
+  const buttonRef = useRef(null);
+  const articleRef = useRef(null);
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    const button = buttonRef.current;
+    const article = articleRef.current;
+
+    const handleClick = () => {
+      setIsActive(!isActive);
+    };
+
+    if (button) {
+      button.addEventListener("click", handleClick);
+    }
+
+    return () => {
+      if (button) {
+        button.removeEventListener("click", handleClick);
+      }
+    };
+  }, [isActive]);
+
+  return (
+    <nav className="burger">
+      <section className="burger-section">
+        <a className="burger-logo" href="/">
+          Energy
+        </a>
+
+        {
+            isActive ? 
+            <article className={`burger-article animate__animated ${isActive ? 'animate__fadeIn' : 'animate__fadeOut'}`} ref={articleRef}>
+          <ul className="burger--ul">
+            <li className="burger--li">
+              <a className="burger--link" href="/">
+                Inicio
+              </a>
+            </li>
+            <li className="burger--li">
+              <a className="burger--link" href="#product">
+                Productos
+              </a>
+            </li>
+            <li className="burger--li">
+              <a className="burger--link" href="about">
+                Sobre Nosotros
+              </a>
+            </li>
+            <li className="burger--li">
+              <a className="burger--link" href="contact">
+                Contactos
+              </a>
+            </li>
+          </ul>
+        </article> 
+        :
+        null
+        }
+        
+
+        <button
+          className={`hamburger hamburger--spring ${isActive ? 'is-active' : ''}`}
+          type="button"
+          ref={buttonRef}
+        >
+          <span className="hamburger-box">
+            <span className="hamburger-inner"></span>
+          </span>
+        </button>
+      </section>
+    </nav>
+  );
+}
+
+export default Navbar;
